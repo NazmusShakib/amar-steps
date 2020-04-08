15b1c697635c1171f8712306ccf9755a5385e771
Nazmus Shakib <nshakib.se@gmail.com>
2020-04-08 19:37:40 +0600

    Added post model ctrl migration files

---
+++ database/migrations/2020_04_08_191243_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumbnail')->nullable();
+
+            $table->enum('status', ['published', 'draft', 'unpublished'])
+                ->nullable();
+
+            $table->bigInteger('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')
+                ->on('users')->onDelete('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
